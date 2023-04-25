de6ecd2db0fd0c8ddc0bca32387f56bca5683e3a
devMicl <dev.yesi3720303@gmail.com>
2023-04-25 19:49:19 +0900

    login 백엔드

--- mealkitLand/src/com/mealkitland/user/dao/UserDAO.java
+++ mealkitLand/src/com/mealkitland/user/dao/UserDAO.java
@@ -1,5 +1,27 @@
 package com.mealkitland.user.dao;
 
-public class UserDAO {
+import java.util.HashMap;
 
+import org.apache.ibatis.session.SqlSession;
+
+import com.mybatis.config.MyBatisConfig;
+
+public class UserDAO {
+	public SqlSession sqlSession;
+	
+	public UserDAO() {
+		sqlSession = MyBatisConfig.getSqlSessionFactory().openSession(true);
+	}
+	
+	//	로그인
+	public Long login(String userIdentification, String userPassword) {
+		HashMap<String, String> loginMap = new HashMap<String, String>();
+		loginMap.put("userIdentification", userIdentification);
+		loginMap.put("userPassword", userPassword);
+		
+		return sqlSession.selectOne("user.login", loginMap);
+		
+	}
+	
+	
 }
