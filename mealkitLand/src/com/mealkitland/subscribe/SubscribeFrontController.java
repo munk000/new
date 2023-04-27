bedeb57835f2bddf1a608d8daa75a733a58fd0fb
kyjoong <duswnd30@gmail.com>
2023-04-27 18:26:03 +0900

    jsp first

--- mealkitLand/src/com/mealkitland/subscribe/SubscribeFrontController.java
+++ mealkitLand/src/com/mealkitland/subscribe/SubscribeFrontController.java
@@ -7,11 +7,37 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.mealkitland.Result;
+import com.mealkitland.subscribe.controller.SubscribeOkController;
+import com.mealkitland.user.controller.LoginOkController;
+
 public class SubscribeFrontController extends HttpServlet {
 @Override
 protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+	req.setCharacterEncoding("UTF-8");
+	
+	String target = req.getRequestURI().replace(req.getContextPath() + "/", "").split("\\.")[0];
+	Result result = null;
+	
+	
+	if(target.equals("subscribeOk")) {
+		result = new SubscribeOkController().execute(req, resp);
+	}
+	
+	
+	
+	if(result != null) {
+		if(result.isRedirect()){
+			resp.sendRedirect(result.getPath());
+		}else {
+			req.getRequestDispatcher(result.getPath()).forward(req, resp);
+		}
+	}
 
 }
+
+	
+
 @Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 	doGet(req, resp);
